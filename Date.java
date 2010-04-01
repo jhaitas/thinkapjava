5db29c650829ae6b3b7c5a3d75722fac9a4497d0
John Haitas <john@haitas.com>
2010-04-01 18:57:58 -0500

    Exercise 2.1

---
+++ Date.java
@@ -0,0 +1,14 @@
+class Date {
+	public static void main(String[] args) {
+		String day;
+		int date, month, year;
+		day = "Thursday";
+		date = 1;
+		month = 4;
+		year = 2010;
+		System.out.println(day);
+		System.out.println(date);
+		System.out.println(month);
+		System.out.println(year);
+	}
+}
