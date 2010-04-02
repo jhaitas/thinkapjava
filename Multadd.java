8b0676903d017c80b39e6f31cb206c4b45773a2a
John Haitas <john@haitas.com>
2010-04-01 19:54:35 -0500

    Exercise 2.5 c

---
+++ Multadd.java
@@ -0,0 +1,9 @@
+class Multadd {
+	public static void multadd(double a, double b, double c) {
+		System.out.printf("%f\n",a*b+c);
+	}
+
+	public static void main(String[] args) {
+		multadd(1.0,2.0,3.0);
+	}
+}
