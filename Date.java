6061a5611816b8358e40e86d71753d15d94b3858
John Haitas <john@haitas.com>
2010-04-01 19:05:59 -0500

    Exercise 2.1 e

--- Date.java
+++ Date.java
@@ -6,6 +6,9 @@ class Date {
 		date = 1;
 		month = "April";
 		year = 2010;
+		System.out.printf("American format:\n");
 		System.out.printf("%s, %s %d, %d\n",day,month,date,year);
+		System.out.printf("European format:\n");
+		System.out.printf("%s %d %s, %d\n",day,date,month,year);
 	}
 }
