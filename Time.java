b79bc0e23a8c9b866122391cb606edb61bafb19a
John Haitas <john@haitas.com>
2010-04-01 19:23:56 -0500

    Exercise 2.2 e

--- Time.java
+++ Time.java
@@ -1,12 +1,17 @@
 class Time {
 	public static void main(String[] args) {
-		int hour, minute, second, secondsSinceMidnight;
+		int hour, minute, second;
+		int secondsSinceMidnight, secondsRemainingInDay;
+		double percentageDayPassed;
 		hour = 19;
 		minute = 8;
 		second = 7;
 		System.out.printf("%02d:%02d:%02d\n",hour,minute,second);
 		secondsSinceMidnight = second + (60 * minute) + (3600 * hour);
 		System.out.printf("Seconds since midnight: %d\n",secondsSinceMidnight);
-		System.out.printf("Seconds remaining in the day: %d\n",86400-secondsSinceMidnight);
+		secondsRemainingInDay = 86400-secondsSinceMidnight;
+		System.out.printf("Seconds remaining in the day: %d\n",secondsRemainingInDay);
+		percentageDayPassed = ( secondsSinceMidnight / 86400.0 ) * 100;
+		System.out.printf("Percentage of day passed: %.2f%%\n",percentageDayPassed);
 	}
 }
