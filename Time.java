96881c473f946d802532c8345ea0bb4f5bc048d3
John Haitas <john@haitas.com>
2010-04-01 19:11:00 -0500

    Exercise 2.2 b

---
+++ Time.java
@@ -0,0 +1,9 @@
+class Time {
+	public static void main(String[] args) {
+		int hour, minute, second;
+		hour = 19;
+		minute = 8;
+		second = 7;
+		System.out.printf("%02d:%02d:%02d\n",hour,minute,second);
+	}
+}
