10228b7daf3aa4585119ce85077cebb8eff10e91
John Haitas <john@haitas.com>
2010-04-01 19:14:56 -0500

    Exercise 2.2 c

--- Time.java
+++ Time.java
@@ -1,9 +1,11 @@
 class Time {
 	public static void main(String[] args) {
-		int hour, minute, second;
+		int hour, minute, second, secondsSinceMidnight;
 		hour = 19;
 		minute = 8;
 		second = 7;
 		System.out.printf("%02d:%02d:%02d\n",hour,minute,second);
+		secondsSinceMidnight = second + (60 * minute) + (3600 * hour);
+		System.out.printf("Seconds since midnight: %d\n",secondsSinceMidnight);
 	}
 }
